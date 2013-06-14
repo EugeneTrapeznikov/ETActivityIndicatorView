c648089782b873514ed7e587a74228030bad0e42
DreadlockEugene <dreadlockeugene@gmail.com>
2013-06-14 16:24:08 +0600

    better structure for ETActivityIndicatorView

--- example/ETActivityIndicatorView/ETActivityIndicatorView/Circle.m
+++ example/ETActivityIndicatorView/ETActivityIndicatorView/Circle.m
@@ -30,12 +30,9 @@
     // Drawing code
     
     CGContextRef context = UIGraphicsGetCurrentContext();
-    //CGContextSetLineWidth(context, 5.0);
-    
     [color set];
     CGContextFillEllipseInRect(context, CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, self.frame.size.height));
     CGContextAddArc(context, self.frame.size.width/2.0, self.frame.size.height/2.0, self.frame.size.height/2.0 -1.0, 0.0, 2.0 * M_PI, YES);
-    //CGContextStrokePath(context);
     CGContextDrawPath(context, kCGPathFill);
 }
 
